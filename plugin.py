2006b1de8f6249437546f69f516d41a0696e4701
patchesyar <johnlavoi@gmail.com>
2014-04-22 16:12:59 -0400

    Committed alpha of predictions
    The current alpha build of plugins.py with prediction functions (that currently do not work). In time the plugin will be able to enable and disable collecting predictions from chat, clear predictions, and check for matches.

--- plugin.py
+++ plugin.py
@@ -11,12 +11,17 @@ post it as a message in chat.
 to-do:
 add an alt-case for players to redirect twitch usernames to steamurls. text document
 add a confirmation notice if the lobby info is more than a day old
+
+Note: Currently the prediction functions do not... function. This is meant to update current progress on the
+program
 """
 
 from urllib.request import *
 from plugins.BasePlugin import BasePlugin
 
 class PystatsPlugin(BasePlugin):
+    predsOn= True
+    predslist=[]
     def __init__(self, twitchy):
         """
         initializes pystats plugin object with the commands
@@ -25,6 +30,67 @@ class PystatsPlugin(BasePlugin):
 
         #self.registerCommand('stats', self.sizzlerHandler)
         self.registerCommand('stats', self.placeholder)
+        self.registerCommand('subtest', self.subtestHandler)
+        """
+        self.registerCommand('predstart', self.predStarter)
+        self.registerCommand('predstop', self.predStopper)
+        self.registerCommand('pred', self.predIntake)
+        self.registerCommand('predcheck', self.predChecker)
+        self.registerCommand('predlist', self.predLister)
+        #remember to clear predslist in predcheck
+"""
+    def predStart(self, nick, commandArg):
+        if nick== 'tomdiamond' or nick== 'raysfire' or nick== 'twilitlord':
+            print("predictions are enabled")
+            #global predsOn=True
+
+    def predStop(self, nick, commandArg):
+        if nick== 'tomdiamond' or nick== 'raysfire' or nick=='twilitlord;':
+            print("predictions are disabled")
+            #global predsOn=False
+
+    def pred(self, nick, commandArg):
+        if predsOn:
+            print(nick+" has made a prediction")
+            if len(commandArg)==2:
+                count=0
+                for element in predslist:
+                    if element[0]== nick:
+                        global predslist.remove(count)
+                    count+=count
+                global predslist.append((nick, commandArg[1]))
+            else:
+                self.sendMessage("Hey "+nick+", you can make a prediction with '!pred x:y'")
+            
+    def predChecker(self, nick, commandArg):
+        if nick== 'tomdiamond' or nick== 'raysfire' or nick=='twilitlord;':
+            print(nick+" is checking the preds"):
+            if len(commandArg)!=2:
+                self.sendMessage("Hey "+nick+", remember to add the correct kd too!")
+            else:
+                chickendinner=False
+                for element in predslist:
+                    if predslist[1]== commandArg[1]:
+                        self.sendMessage(predslist[0]+" correctly guessed the kd and won the prediction contest!")
+                        chickendinner=True
+                if not chickendinner:
+                    self.sendMessasge("Nobody won the prediction contest this time")
+                global predslist=[]
+
+    def predlist(self, nick, commandArg):
+        if len(commandArg)==1:
+            predmakers=""
+            for element in global predslist:
+                predmakers+=(element[0]+" ")
+            self.sendMessage(predmakers) #returns the list of all the people that made predictions
+        else:
+            for element in global predslist:
+                if element[0]==commandArg[1]:
+                    self.sendMessage(element[1]) #returns the kd prediction for a certain player
+                    
+
+    def subtestHandler(self, nick, commandArg):
+        self.sendMessage("I got "+ findSubstitutes())
 
     def placeholder(self, nick, commandArg):
         """
@@ -45,7 +111,7 @@ class PystatsPlugin(BasePlugin):
             steamID=commandArg[1]
         self.sendMessage(sizzlerGet(steamID))
 
-    def sizzlerGet(steamID=self.Twitch_Channel):
+    def sizzlerGet(steamID):
         """
         given a steam ID in any format, retrieves the latest sizzling stats lobby
         link and returns it
@@ -74,3 +140,13 @@ class PystatsPlugin(BasePlugin):
         with urlopen(pagetitle) as url:
             s=url.read
         print(s)
+
+    def findSubstitutes():
+        twitchname
+        for line in open("substitutes.txt"):
+            inline=line.split(" ")
+            if twitchname!=inline[0]:
+                break
+            else:
+                return inline[1]
+        return twitchname
